708b30f6a423735730d8b737361c517622a9b4f9
Claudio Pupparo <claudio.pupparo@gmail.com>
2014-08-15 11:59:17 +0200

    MGOGRelationship with its primary key and unidirectional OneToOne mapping to Goal (MG and OG)

--- src/main/java/it/uniroma2/gqm/model/MGOGRelationship.java
+++ src/main/java/it/uniroma2/gqm/model/MGOGRelationship.java
@@ -1,29 +1,32 @@
 package it.uniroma2.gqm.model;
 
-import javax.persistence.AssociationOverride;
-import javax.persistence.AssociationOverrides;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.appfuse.model.BaseObject;
 
 @Entity
 @Table(name = "mg_og_relationship")
-@AssociationOverrides({ @AssociationOverride(name = "pk.mg", joinColumns = @JoinColumn(name = "mg_id")),
-						@AssociationOverride(name = "pk.og", joinColumns = @JoinColumn(name = "og_id")) })
 @NamedQueries({
-    @NamedQuery(
-            name = "findMGOGRelationship",
-            query = "select mgog from MGOGRelationship mgog  where mgog.pk.mg.id= :mg_id and  mgog.pk.og.id = :og_id"
-    )
+	@NamedQuery(
+			name = "findMGOGRelationship",
+			query = "select mgog from MGOGRelationship mgog " +
+					"where mgog.pk.mgID = :mg_id and mgog.pk.ogID = :og_id"
+	)
 })
 public class MGOGRelationship extends BaseObject {
 
+	private static final long serialVersionUID = 6535760101622612386L;
+
 	private MGOGRelationshipPK pk;
+	
+	private Goal mg;
+	private Goal og;
 	
 	@EmbeddedId
 	public MGOGRelationshipPK getPk() {
@@ -34,10 +37,34 @@ public class MGOGRelationship extends BaseObject {
 		this.pk = pk;
 	}
 
+	//associazione unidirezionale (in Goal non sono presenti riferimenti a questa entità)
+	@MapsId("mgID")
+	@OneToOne
+	public Goal getMg() {
+		return mg;
+	}
+
+	public void setMg(Goal mg) {
+		this.mg = mg;
+	}
+
+	//associazione unidirezionale (in Goal non sono presenti riferimenti a questa entità)
+	@MapsId("ogID")
+	@OneToOne
+	public Goal getOg() {
+		return og;
+	}
+
+	public void setOg(Goal og) {
+		this.og = og;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((mg == null) ? 0 : mg.hashCode());
+		result = prime * result + ((og == null) ? 0 : og.hashCode());
 		result = prime * result + ((pk == null) ? 0 : pk.hashCode());
 		return result;
 	}
@@ -51,6 +78,16 @@ public class MGOGRelationship extends BaseObject {
 		if (getClass() != obj.getClass())
 			return false;
 		MGOGRelationship other = (MGOGRelationship) obj;
+		if (mg == null) {
+			if (other.mg != null)
+				return false;
+		} else if (!mg.equals(other.mg))
+			return false;
+		if (og == null) {
+			if (other.og != null)
+				return false;
+		} else if (!og.equals(other.og))
+			return false;
 		if (pk == null) {
 			if (other.pk != null)
 				return false;
@@ -61,8 +98,9 @@ public class MGOGRelationship extends BaseObject {
 
 	@Override
 	public String toString() {
-		return "MGOGRelationship [pk=" + pk + "]";
+		return "MGOGRelationship [pk=" + pk + ", mg=" + mg + ", og=" + og + "]";
 	}
-
+	
+	
 
 }
--- src/main/java/it/uniroma2/gqm/model/MGOGRelationshipPK.java
+++ src/main/java/it/uniroma2/gqm/model/MGOGRelationshipPK.java
@@ -3,41 +3,36 @@ package it.uniroma2.gqm.model;
 import java.io.Serializable;
 
 import javax.persistence.Embeddable;
-import javax.persistence.ManyToOne;
 
 @Embeddable
 public class MGOGRelationshipPK implements Serializable {
 
 	private static final long serialVersionUID = -2870344507198161817L;
-	private Goal mg;
-	private Goal og;
 	
-	@ManyToOne
-	public Goal getMg() {
-		return mg;
-	}
-	public void setMg(Goal mg) {
-		this.mg = mg;
-	}
-	 
-	@ManyToOne
-	public Goal getOg() {
-		return og;
-	}
+	//chiavi dei due Goal puntati dalle due foreign key
+	private Long mgID;
+	private Long ogID;	
 	
-	public void setOg(Goal og) {
-		this.og = og;
+	public Long getMgID() {
+		return mgID;
 	}
-	
+	public void setMgID(Long mgID) {
+		this.mgID = mgID;
+	}
+	public Long getOgID() {
+		return ogID;
+	}
+	public void setOgID(Long ogID) {
+		this.ogID = ogID;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((mg == null) ? 0 : mg.hashCode());
-		result = prime * result + ((og == null) ? 0 : og.hashCode());
+		result = prime * result + ((mgID == null) ? 0 : mgID.hashCode());
+		result = prime * result + ((ogID == null) ? 0 : ogID.hashCode());
 		return result;
 	}
-	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -47,21 +42,22 @@ public class MGOGRelationshipPK implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		MGOGRelationshipPK other = (MGOGRelationshipPK) obj;
-		if (mg == null) {
-			if (other.mg != null)
+		if (mgID == null) {
+			if (other.mgID != null)
 				return false;
-		} else if (!mg.equals(other.mg))
+		} else if (!mgID.equals(other.mgID))
 			return false;
-		if (og == null) {
-			if (other.og != null)
+		if (ogID == null) {
+			if (other.ogID != null)
 				return false;
-		} else if (!og.equals(other.og))
+		} else if (!ogID.equals(other.ogID))
 			return false;
 		return true;
 	}
-	
 	@Override
 	public String toString() {
-		return "MGOGRelationshipPK [mg=" + mg + ", og=" + og + "]";
-	}	
+		return "MGOGRelationshipPK [mgID=" + mgID + ", ogID=" + ogID + "]";
+	}
+	
+	
 }
