ecb98cb2f776fad08460e51efc40c84b16b39e1c
think3r <tankhack@gmail.com>
2014-08-25 23:58:23 +0200

    String json grafico Grid OK

--- src/main/java/it/uniroma2/gqm/service/GridManagerImpl.java
+++ src/main/java/it/uniroma2/gqm/service/GridManagerImpl.java
@@ -1,11 +1,12 @@
 package it.uniroma2.gqm.service;
 
-import java.util.Set;
-
 import it.uniroma2.gqm.model.Goal;
 import it.uniroma2.gqm.model.Strategy;
 
+import java.util.Set;
+
 import org.appfuse.service.impl.GenericManagerImpl;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 @Service("gridManager")
@@ -68,4 +69,97 @@ public class GridManagerImpl extends GenericManagerImpl<Goal, Long> implements G
 
     	return false;
     }
+	
+	public String explorer(Object obj, String s){
+		
+		String tree = "{";
+		
+		tree += "\"name\":";
+		if(obj instanceof Goal) {
+    		Goal g = (Goal) obj;
+    		
+    		tree += "\""+g.getDescription()+"\"";
+    		tree += ",\"parent\":";
+    		tree += "\""+s+"\"";
+    		if(g.hasChildren()) { //ha figli
+    			
+    			tree += ", \"children\":[";
+    			
+    			if(g.areChildrenGoal()) { //i figli sono goal
+    				Set<Goal> children = g.getOrgChild();
+    				int i = 0;
+    				for (Goal goal : children) {
+    					if(i != 0)
+    						tree += ",";
+    					tree += explorer(goal, g.getDescription());
+    					i++;
+    					
+    				}
+    				tree += "]}";
+    				
+    			} else { //i figli sono strategy
+    				Set<Strategy> children = g.getOstrategyChild();
+    				int i = 0;
+    				for (Strategy strategy : children) {
+    					if(i != 0)
+    						tree += ",";
+    					tree += explorer(strategy, g.getDescription());
+    					i++;
+    				}
+    				tree += "]}";
+    			}
+    			
+    		} else { //non ha figli
+    			tree += "}";
+    		}
+    		
+    	} else if(obj instanceof Strategy) {
+    		Strategy stra = (Strategy)obj;
+    		
+    		tree += "\""+stra.getName()+"\"";
+    		tree += ",\"parent\":";
+    		tree += "\""+s+"\"";
+    		if(stra.hasChildren()) { //ha figli
+    			
+    			tree += ", \"children\":[";
+    			
+    			if(stra.areChildrenGoal()) { //i figli sono goal
+    				Set<Goal> children = stra.getSorgChild();
+    				int i = 0;
+    				for (Goal goal : children) {
+    					if(i != 0)
+    						tree += ",";
+    					tree += explorer(goal, stra.getName());
+    					i++;
+    					
+    				}
+    				tree += "]}";
+    				
+    			} else { //i figli sono strategy
+    				Set<Strategy> children = stra.getStrategyChild();
+    				int i = 0;
+    				for (Strategy strategy : children) {
+    					if(i != 0)
+    						tree += ",";
+    					tree += explorer(strategy, stra.getName());
+    					i++;
+    					
+    				}
+    				tree += "]}";
+    			}
+    			
+    		} else { //non ha figli
+    			tree += "}"; //oppure devo controllare gli id? In caso positivo, devo controllare solo se sono dello stesso tipo
+    		}    		
+    	}
+		
+		//tree += "}";
+		
+		return tree;
+		
+	}
+	
+	public JSONObject saveTreeToJSON(String s){
+		return new JSONObject(s);
+	}
 }
--- src/main/java/it/uniroma2/gqm/webapp/controller/GridController.java
+++ src/main/java/it/uniroma2/gqm/webapp/controller/GridController.java
@@ -1,9 +1,33 @@
 package it.uniroma2.gqm.webapp.controller;
 
 
+import it.uniroma2.gqm.model.Goal;
+import it.uniroma2.gqm.model.GoalStatus;
+import it.uniroma2.gqm.model.GoalType;
+import it.uniroma2.gqm.model.MGOGRelationship;
+import it.uniroma2.gqm.model.Project;
+import it.uniroma2.gqm.model.Strategy;
+import it.uniroma2.gqm.service.GoalManager;
+import it.uniroma2.gqm.service.GridManager;
+import it.uniroma2.gqm.service.MGOGRelationshipManager;
+import it.uniroma2.gqm.service.StrategyManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang.StringUtils;
+import org.appfuse.model.User;
+import org.appfuse.service.GenericManager;
+import org.appfuse.service.UserManager;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -13,9 +37,64 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/grid*")
 @SessionAttributes({"currentProject","goal","currentUser"})
 public class GridController {
+	
+	 @Autowired
+    private GoalManager goalManager;
+    
+    private UserManager userManager = null;
 
+    private GenericManager<Project, Long> projectManager = null;
+    
+    @Autowired
+    private StrategyManager strategyManager;
+
+    private MGOGRelationshipManager mgogRelationshipManager;
+    
+    private GridManager gridManager;
+    
+    @Autowired
+    public void setGridManager(@Qualifier("gridManager") GridManager gridManager) {
+    	this.gridManager = gridManager;
+    }
+    
+    @Autowired
+	public void setMgogRelationshipManager(@Qualifier("mgogRelationshipManager") MGOGRelationshipManager mgogRelationshipManager) {
+		this.mgogRelationshipManager = mgogRelationshipManager;
+	}
+
+	@Autowired
+    public void setProjectManager(@Qualifier("projectManager") GenericManager<Project, Long> projectManager) {
+        this.projectManager = projectManager;
+    }
+    
+    @Autowired
+    public void setGoalManager(@Qualifier("goalManager") GoalManager goalManager) {
+        this.goalManager = goalManager;
+    }
+    
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView handleRequest(HttpSession session) throws Exception {
-		return new ModelAndView();
+	
+        Goal ret = goalManager.get(Long.parseLong("1"));
+        String tree = gridManager.explorer(ret, "null");
+		System.out.println(tree);
+		JSONObject tstring = new JSONObject(tree);
+		
+		return new ModelAndView().addObject("tree", tstring);
 	}
+	/*
+	@ModelAttribute
+    @RequestMapping(method = RequestMethod.GET)
+    protected Goal showForm(HttpServletRequest request,HttpSession session, Model model) throws Exception {
+        
+		
+		
+//		model.addAttribute("currentUser",currentUser);
+//		model.addAttribute("visibleGESection",visibleGESection);
+//		model.addAttribute("modificableHeader",modificableHeader);
+//        model.addAttribute("availableStatus",availableStatus);
+        
+        return ret;
+    }
+    */
 }
